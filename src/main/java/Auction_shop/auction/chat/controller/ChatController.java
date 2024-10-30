e8dec3b66ce72804ae795f19d795ce5e4e02d69b
hyundoo <hyundoo1006@gmail.com>
2024-10-30 17:35:05 +0900

    feat: 채팅 보낼 시 상대방에게 푸시알림 전송

--- src/main/java/Auction_shop/auction/chat/controller/ChatController.java
+++ src/main/java/Auction_shop/auction/chat/controller/ChatController.java
@@ -3,6 +3,9 @@ package Auction_shop.auction.chat.controller;
 import Auction_shop.auction.chat.domain.Chat;
 import Auction_shop.auction.chat.dto.ChatDto;
 import Auction_shop.auction.chat.service.ChatService;
+import Auction_shop.auction.chatRoom.service.ChatRoomService;
+import Auction_shop.auction.domain.member.Member;
+import Auction_shop.auction.web.fcm.service.FirebaseCloudMessageService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -15,11 +18,15 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatService chatService;
+    private final FirebaseCloudMessageService fcmService;
+    private final ChatRoomService chatRoomService;
 
     @Autowired
-    public ChatController(SimpMessagingTemplate messagingTemplate, ChatService chatService) {
+    public ChatController(SimpMessagingTemplate messagingTemplate, ChatService chatService, FirebaseCloudMessageService fcmService, ChatRoomService chatRoomService) {
         this.messagingTemplate = messagingTemplate;
         this.chatService = chatService;
+        this.fcmService = fcmService;
+        this.chatRoomService = chatRoomService;
     }
 
     @MessageMapping("/chatroom/{roomId}")
@@ -37,6 +44,12 @@ public class ChatController {
 
         try {
             messagingTemplate.convertAndSend(destination, response);   // 도착 경로로 메세지 전달
+            Member member = chatRoomService.findMemberByChatRoom(Long.parseLong(roomId));
+
+            //채팅 받는 상대에게 푸시알림
+            fcmService.sendMessageTo(member.getDeviceToken(),
+                    "채팅 도착!",
+                    chat.getMessage());
         } catch (Exception e) {
             log.error("ChatError = {}", e);
             messagingTemplate.convertAndSend(destination, "Error:" + e.getMessage());
